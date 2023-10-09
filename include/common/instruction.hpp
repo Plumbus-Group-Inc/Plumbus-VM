84e94f148770a777bf16e496bc156da82883f751
Ilya Mamay <55717770+mmamayka@users.noreply.github.com>
2023-10-09 23:37:12 +0300

    [common] instruction definition (#1)
    * [common] instruction definition
    
    * [common] Add default values to instruction structure fields
    
    * [common] Rename ALU3 -> ALU_BINARY, ALU2 -> ALU_UNARY

---
+++ include/common/instruction.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <cstdint>
+
+namespace pvm {
+
+enum : uint8_t {
+    OPCODE_HALT = 0,
+    OPCODE_LOAD_IMM_I,
+    OPCODE_LOAD_IMM_F,
+    OPCODE_ALU_BINARY,
+    OPCODE_ALU_UNARY,
+    OPCODE_IO,
+    OPCODE_BRANCH
+};
+
+// Type bit
+enum : uint32_t {
+    OP_TYPE_I = 0,
+    OP_TYPE_F = 1
+};
+
+// ALU binary raw operations
+enum : uint32_t {
+    OP_ADD_I = 0,
+    OP_ADD_F,
+    OP_SUB_I,
+    OP_SUB_F,
+    OP_MUL_I,
+    OP_MUL_F,
+    OP_DIV_I,
+    OP_DIV_F
+};
+
+// ALU binary type independent operations
+enum : uint32_t {
+    OP_ADD = 0,
+    OP_SUB = 2,
+    OP_MUL = 4,
+    OP_DIV = 6
+};
+
+// ALU unary raw operations
+enum : uint32_t {
+    OP_SQRT_I = 0,
+    OP_SQRT_F,
+    OP_CAST_ITOF,
+    OP_CAST_FTOI,
+    OP_ABS_I,
+    OP_ABS_F
+};
+
+// ALU unary type independent operations
+enum : uint32_t {
+    OP_SQRT = 0,
+    OP_CAST = 2,
+    OP_ABS = 4,
+};
+
+// IO raw operations
+enum : uint32_t {
+    OP_READ_I = 0,
+    OP_READ_F,
+    OP_WRITE_I,
+    OP_WRITE_F
+};
+
+// IO type independent operations
+enum : uint32_t {
+    OP_READ = 0,
+    OP_WRITE = 2
+};
+
+// Branch raw operations
+enum : uint32_t {
+    OP_BEQUAL_I = 0,
+    OP_BEQUAL_F,
+    OP_BLESS_I,
+    OP_BLESS_F
+};
+
+// Branch type independent operations
+enum : uint32_t {
+    OP_BEQUAL = 0,
+    OP_BLESS = 2
+};
+
+struct Instruction {
+    uint8_t opcode = 0;
+
+    uint8_t rd = 0;
+    uint8_t rs1 = 0;
+    uint8_t rs2 = 0;
+
+    union {
+        uint32_t op;
+        int32_t immi;
+        float immf;
+    };
+};
+
+}
