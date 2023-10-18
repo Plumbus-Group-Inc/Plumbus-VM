64a883ede59f5f00f923b7724f314afa6adbbd92
Tako-San <dxe13@mail.ru>
2023-10-18 23:31:35 +0300

    Added interpreter class

---
+++ include/interpreter/interpreter.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "decoder/decoder.hpp"
+#include "executor/executor.hpp"
+
+namespace pvm {
+
+class Interpreter final {
+public:
+  void run();
+
+private:
+  Decoder m_dec;
+  Executor m_exec;
+  // memory
+  // pc
+};
+
+}
