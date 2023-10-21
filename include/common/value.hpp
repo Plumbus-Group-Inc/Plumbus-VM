5791f2a316aa5c1a2fdda80f924d91d540e592c6
mmamayka <mr.green4000@gmail.com>
2023-10-21 20:09:25 +0300

    Move method declarations out of class Value

--- include/common/value.hpp
+++ include/common/value.hpp
@@ -30,44 +30,72 @@ public:
 
     template<typename Type>
     requires ListedType<Type, Types...>
-    [[nodiscard]] Type read() const {
-        auto pvalue = std::get_if<Type>(&m_data);
-        if(pvalue == nullptr) {
-            throw ValueMismatchError(typeid(Type), this->currentTypeinfo());
-        }
-
-        return *pvalue;
-    }
+    [[nodiscard]] Type read() const;
 
     template<typename Type>
     requires ListedType<Type, Types...>
-    void write(Type value) {
-        if(!this->holds<Type>()) {
-            throw ValueMismatchError(typeid(Type), this->currentTypeinfo());
-        }
-
-        m_data = value;
-    }
+    void write(Type value);
 
     template<typename Type>
     requires ListedType<Type, Types...>
-    void overwrite(Type value) {
-        m_data = value;
-    }
+    void overwrite(Type value);
 
     template<typename Type>
     requires ListedType<Type, Types...>
-    [[nodiscard]] bool holds() const noexcept {
-        return std::holds_alternative<Type>(m_data);
-    }
+    [[nodiscard]] bool holds() const noexcept;
 
 private:
-    [[nodiscard]] std::type_info const& currentTypeinfo() const noexcept {
-        return VariadicNthInfo<Types...>(m_data.index());
-    }
+    [[nodiscard]] std::type_info const& currentTypeinfo() const noexcept;
 
     ValueVariant m_data;
 };
+
+template<typename... Types>
+requires UniqTypesList<Types...>
+template<typename Type>
+requires ListedType<Type, Types...>
+[[nodiscard]] Type ValueImpl<Types...>::read() const {
+    auto pvalue = std::get_if<Type>(&m_data);
+    if(pvalue == nullptr) {
+        throw ValueMismatchError(typeid(Type), this->currentTypeinfo());
+    }
+
+    return *pvalue;
+}
+
+template<typename... Types>
+requires UniqTypesList<Types...>
+template<typename Type>
+requires ListedType<Type, Types...>
+void ValueImpl<Types...>::write(Type value) {
+    if(!this->holds<Type>()) {
+        throw ValueMismatchError(typeid(Type), this->currentTypeinfo());
+    }
+
+    m_data = value;
+}
+
+template<typename... Types>
+requires UniqTypesList<Types...>
+template<typename Type>
+requires ListedType<Type, Types...>
+void ValueImpl<Types...>::overwrite(Type value) {
+    m_data = value;
+}
+
+template<typename... Types>
+requires UniqTypesList<Types...>
+template<typename Type>
+requires ListedType<Type, Types...>
+[[nodiscard]] bool ValueImpl<Types...>::holds() const noexcept {
+    return std::holds_alternative<Type>(m_data);
+}
+
+template<typename... Types>
+requires UniqTypesList<Types...>
+[[nodiscard]] std::type_info const& ValueImpl<Types...>::currentTypeinfo() const noexcept {
+    return VariadicNthInfo<Types...>(m_data.index());
+}
 
 template<typename Type>
 concept ValueT = VariadicHasType<Type, int, float>;
