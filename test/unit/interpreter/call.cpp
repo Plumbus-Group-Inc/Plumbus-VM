dc9f2663909c4dc94e1f64e2cc62011d82b8e1cc
Tako-San <dxe13@mail.ru>
2023-12-19 05:39:37 +0300

    Fix test call failure

--- test/unit/interpreter/call.cpp
+++ test/unit/interpreter/call.cpp
@@ -18,15 +18,15 @@ TEST(Interpreter, Call) {
         /* 0 */ Instr{.opType = eIMM, .opID = eIMM_INTEGER, .instrVar =
             InstrIMM::Builder().data(5).build()},
         /* 1 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(0).build()},
+            InstrREG::Builder().regid(0 + 1).build()},
         /* 2 */ Instr{.opType = eBINARY, .opID = eBINARY_EQUAL, .instrVar =
-            InstrBINARY::Builder().regid1(0).regid2(0).ttypeid(kInt).build()},
+            InstrBINARY::Builder().regid1(0 + 1).regid2(0 + 1).ttypeid(kInt).build()},
         /* 3 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(1).build()},
+            InstrREG::Builder().regid(1 + 1).build()},
         /* 4 */ Instr{.opType = eBRANCH, .opID = eBRANCH_CALL,
-            .instrVar = InstrBRANCH::Builder().regid(1).offset(3).build()},
+            .instrVar = InstrBRANCH::Builder().regid(1 + 1).offset(3).build()},
         /* 5 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(0).build()},
+            InstrREG::Builder().regid(0 + 1).build()},
         /* 6 */ Instr{.opType = eHALT, .opID = 0, .instrVar =
             InstrHALT::Builder().zero(0).build()},
 
@@ -34,55 +34,55 @@ TEST(Interpreter, Call) {
         /* 7 */ Instr{.opType = eIMM, .opID = eIMM_INTEGER, .instrVar =
             InstrIMM::Builder().data(0).build()},
         /* 8 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(1).build()},
+            InstrREG::Builder().regid(1 + 1).build()},
         /* 9 */ Instr{.opType = eBINARY, .opID = eBINARY_EQUAL, .instrVar =
-            InstrBINARY::Builder().regid1(0).regid2(1).ttypeid(kInt).build()},
+            InstrBINARY::Builder().regid1(0 + 1).regid2(1 + 1).ttypeid(kInt).build()},
         /* 10 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(2).build()},
+            InstrREG::Builder().regid(2 + 1).build()},
         /* 11 */ Instr{.opType = eBRANCH, .opID = eBRANCH_BRANCH, .instrVar =
-            InstrBRANCH::Builder().regid(2).offset(16).build()},
+            InstrBRANCH::Builder().regid(2 + 1).offset(16).build()},
 
         // Wanna save R0 parameter value for future to R2
         /* 12 */ Instr{.opType = eBINARY, .opID = eBINARY_ADD, .instrVar =
-            InstrBINARY::Builder().ttypeid(kInt).regid1(0).regid2(1).build()},
+            InstrBINARY::Builder().ttypeid(kInt).regid1(0 + 1).regid2(1 + 1).build()},
         /* 13 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(2).build()},
+            InstrREG::Builder().regid(2 + 1).build()},
 
         // Wanna decrement R0
         /* 14 */ Instr{.opType = eIMM, .opID = eIMM_INTEGER, .instrVar =
             InstrIMM::Builder().data(-1).build()},
         /* 15 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(1).build()},
+            InstrREG::Builder().regid(1 + 1).build()},
         /* 16 */ Instr{.opType = eBINARY, .opID = eBINARY_ADD, .instrVar =
-            InstrBINARY::Builder().ttypeid(kInt).regid1(0).regid2(1).build()},
+            InstrBINARY::Builder().ttypeid(kInt).regid1(0 + 1).regid2(1 + 1).build()},
         /* 17 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(0).build()},
+            InstrREG::Builder().regid(0 + 1).build()},
 
         // Perform recursive call of this function
         /* 18 */ Instr{.opType = eBINARY, .opID = eBINARY_EQUAL, .instrVar =
-            InstrBINARY::Builder().regid1(0).regid2(0).ttypeid(kInt).build()},
+            InstrBINARY::Builder().regid1(0 + 1).regid2(0 + 1).ttypeid(kInt).build()},
         /* 19 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(3).build()},
+            InstrREG::Builder().regid(3 + 1).build()},
         /* 20 */ Instr{.opType = eBRANCH, .opID = eBRANCH_CALL,
-            .instrVar = InstrBRANCH::Builder().regid(3).offset(-13).build()},
+            .instrVar = InstrBRANCH::Builder().regid(3 + 1).offset(-13).build()},
 
         // Add R0 input value to result
         /* 21 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(0).build()},
+            InstrREG::Builder().regid(0 + 1).build()},
         /* 22 */ Instr{.opType = eBINARY, .opID = eBINARY_ADD, .instrVar =
-            InstrBINARY::Builder().ttypeid(kInt).regid1(0).regid2(2).build()},
+            InstrBINARY::Builder().ttypeid(kInt).regid1(0 + 1).regid2(2 + 1).build()},
         /* 23 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(0).build()},
+            InstrREG::Builder().regid(0 + 1).build()},
         /* 24 */ Instr{.opType = eBRANCH, .opID = eBRANCH_RET,
-            .instrVar = InstrBRANCH::Builder().regid(0).offset(0).build()},
+            .instrVar = InstrBRANCH::Builder().regid(0 + 1).offset(0).build()},
 
         // Recursion termination
         /* 25 */ Instr{.opType = eIMM, .opID = eIMM_INTEGER, .instrVar =
             InstrIMM::Builder().data(0).build()},
         /* 26 */ Instr{.opType = eREG, .opID = eREG_MOV, .instrVar =
-            InstrREG::Builder().regid(0).build()},
+            InstrREG::Builder().regid(0 + 1).build()},
         /* 27 */ Instr{.opType = eBRANCH, .opID = eBRANCH_RET,
-            .instrVar = InstrBRANCH::Builder().regid(0).offset(0).build()},
+            .instrVar = InstrBRANCH::Builder().regid(0 + 1).offset(0).build()},
     };
     // clang-format on
 
